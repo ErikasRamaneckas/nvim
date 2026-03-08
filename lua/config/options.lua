5cd555714ca3a750b1ba2f448e841efd2348b12d
ErikasRamaneckas <erikasramaneckas@gmail.com>
2026-03-08 10:09:38 +0200

    add real line number, indentation settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 local opt = vim.opt
 
+-- Line number settings
+opt.nu = true
 opt.relativenumber = true
+
+-- Indentation settings
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
