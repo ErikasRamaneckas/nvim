bd1c407e3569afc0e5a9da35980cda9b650edd31
ErikasRamaneckas <erikasramaneckas@gmail.com>
2026-03-14 07:56:19 +0200

    add conform plugin

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+        })
+    end
+}
