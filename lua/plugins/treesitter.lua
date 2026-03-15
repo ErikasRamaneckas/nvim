33509627d8e0ffcdd063930c9b2047ef34f7990d
ErikasRamaneckas <erikasramaneckas@gmail.com>
2026-03-15 07:52:33 +0200

    add treesitter support for php, blade, html, css, js

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,28 @@
+local parsers = {
+    "lua",
+    "php",
+    "php_only",
+    "blade",
+    "html",
+    "css",
+    "javascript",
+}
+
 return {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     lazy = false,
-    build = ':TSUpdate',
-    opts = {
-        ensure_installed = { 'lua' }
-    }
+    branch = "main",
+    build = ":TSUpdate",
+    opts = {},
+    config = function(_, opts)
+        local nts = require("nvim-treesitter")
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = parsers,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+        nts.setup(opts)
+        nts.install(parsers)
+    end,
 }
