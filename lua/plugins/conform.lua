fadd435a1af150b303ede4d3f0371fc3b582344e
ErikasRamaneckas <erikasramaneckas@gmail.com>
2026-04-04 11:41:21 +0300

    add blade formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -7,6 +7,7 @@ return {
             formatters_by_ft = {
                 lua = { "stylua" },
                 php = { "pint", "php_cs_fixer" },
+                blade = { "blade-formatter" },
             },
             format_on_save = {
                 timeout_ms = 500,
