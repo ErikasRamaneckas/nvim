37daba341a1c6e0d7d966e5c66ddf70697fc0692
ErikasRamaneckas <erikasramaneckas@gmail.com>
2026-05-01 09:39:26 +0300

    remove in-built autocomplete options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,8 +11,3 @@ opt.shiftwidth = 4
 opt.expandtab = true
 
 opt.scrolloff = 12
-
--- Autocomplete options
-opt.completeopt = { "menuone", "noselect", "popup" }
-opt.autocomplete = true
-opt.complete:append("s")
