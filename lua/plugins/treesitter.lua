5a1d7373cfef763350a245daead1c45c7601f6e6
ErikasRamaneckas <erikasramaneckas@gmail.com>
2026-05-01 08:30:02 +0300

    clean up treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,14 +11,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
+    init = function()
+        require("nvim-treesitter").install(parsers)
+    end,
     config = function()
-        require("nvim-treesitter").install({ "lua", "php", "blade", "html", "css", "javascript" })
-
         vim.api.nvim_create_autocmd("FileType", {
             pattern = parsers,
             callback = function()
-                vim.treesitter.start()
-                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                vim.treesitter.start()                                            -- highlighting
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- indentation
             end,
         })
     end,
